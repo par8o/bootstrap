3fc6fd50aa497fff1435d7a7682e711e72e2a78e
Nick Ewing <nick@nickewing.net>
2014-02-07 11:43:55 -0800

    Revert "adding optional close button functionality"
    This reverts commit 3b7c52d9839e4ac49d05032077dca7bf3e482e0b.

--- src/timepicker/timepicker.js
+++ src/timepicker/timepicker.js
@@ -21,19 +21,8 @@ angular.module('ui.bootstrap.timepicker', [])
         return; // do nothing if no ng-model
       }
 
-      var close, selected = new Date(),
+      var selected = new Date(),
           meridians = angular.isDefined(attrs.meridians) ? scope.$parent.$eval(attrs.meridians) : timepickerConfig.meridians || $locale.DATETIME_FORMATS.AMPMS;
-
-      scope.closeable = false;
-      if (attrs.close)
-        close = attrs.close;
-
-      if(close) {
-        scope.closeable = true;
-        scope.close = function() {
-          $parse(close)(scope.$parent);
-        };
-      }
 
       var hourStep = timepickerConfig.hourStep;
       if (attrs.hourStep) {
